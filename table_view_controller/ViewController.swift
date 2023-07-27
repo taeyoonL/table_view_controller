a3f89fa994c530f908b1b71ec9620af379f5b7c6
Taeyoon Lee <132141316+taeyoonL@users.noreply.github.com>
2023-07-27 20:24:51 +0900

    Update ViewController.swift

--- table_view_controller/ViewController.swift
+++ table_view_controller/ViewController.swift
@@ -1,19 +1,1 @@
-//
-//  ViewController.swift
-//  table_view_controller
-//
-//  Created by 이태윤 on 2023/07/15.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
 
